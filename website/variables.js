a76b26952f49a739d4ddc35236ad44f2d539aec3
Berret Terry <berretterry@ibm.com>
2023-06-08 07:51:06 -0700

    first website build

---
+++ website/variables.js
@@ -0,0 +1,8 @@
+const Info = {
+    repo: 'https://github.com/ibm-client-engineering/mq-ansible-fork.git',
+    project: 'mq-ansible-fork',
+    org: 'ibm-client-engineering',
+    url: 'https://ibm-client-engineering.github.io',
+    baseUrl: '/mq-ansible-fork/'
+};
+module.exports = { Info };
